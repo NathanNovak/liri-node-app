b3e861492d285e899da0d06df2cb8387df9bb506
NathanNovak <nnovak@ku.edu>
2018-06-28 18:14:14 -0700

    updated liri.js

--- liri.js
+++ liri.js
@@ -1,20 +1,46 @@
 require("dotenv").config();
 
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 console.log(client);
-console.log('----------------------');
+console.log("----------------------");
 console.log(spotify);
 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+
+// spotify.search({ type: 'artist', query: 'Garth Brooks' }, function(err, data, response) {
 //     if (err) {
 //       return console.log('Error occurred: ' + err);
 //     }
-   
-//   console.log(data); 
+
+//   console.log(data);
 //   });
+
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
+
+// var params = {
+//   q: "#my-tweets",
+//   count: 2,
+//   result_type: "recent",
+//   lang: "en"
+// };
+
+// client.get("search/tweets", params, searchedData); // get is the
+// // function to search the tweet which three paramaters 'search/tweets'
+// // ,params and a callback function.
+
+// function searchedData(err, data, response) {
+//   console.log("TWEETS", data);
+// } // searchedData function is a callback function which
+// // returns the data when we make a search
