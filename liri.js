8393f54381f3029e84c9f864dc74994e499d16d7
Nathan Novak <nnovak@ku.edu>
2018-06-29 12:13:27 -0700

    fixed spotify and twitter api request

--- liri.js
+++ liri.js
@@ -12,12 +12,15 @@ console.log("----------------------");
 console.log(spotify);
 
 
-// spotify.search({ type: 'artist', query: 'Garth Brooks' }, function(err, data, response) {
+// spotify.search({ type: 'track', query: 'friends in low places' }, function(err, data, response) {
 //     if (err) {
 //       return console.log('Error occurred: ' + err);
 //     }
 
-//   console.log(data);
+//   console.log("Here's a preview: " + data.tracks.items[0].preview_url);
+//   console.log("Here's the album name: " + data.tracks.items[0].album.name);
+//   console.log("Here's the song name: " + data.tracks.items[0].name);
+//   console.log("Popularity score " + data.tracks.items[0].popularity);
 //   });
 
 // spotify
@@ -29,18 +32,21 @@ console.log(spotify);
 //     console.error('Error occurred: ' + err); 
 //   });
 
-// var params = {
-//   q: "#my-tweets",
-//   count: 2,
-//   result_type: "recent",
-//   lang: "en"
-// };
+var params = {
+  q: 'NathanNovak16'
+};
 
-// client.get("search/tweets", params, searchedData); // get is the
-// // function to search the tweet which three paramaters 'search/tweets'
-// // ,params and a callback function.
+client.get("search/tweets", params, function(error, tweets, response){
+  if (!error){
+    console.log(tweets);
+  }
+}); 
+// get is the function to search the tweet which three paramaters 'search/tweets'
+// ,params and a callback function.
 
 // function searchedData(err, data, response) {
+  
+//   }
 //   console.log("TWEETS", data);
 // } // searchedData function is a callback function which
 // // returns the data when we make a search
